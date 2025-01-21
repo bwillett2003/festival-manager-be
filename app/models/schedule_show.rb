953bc29dd6f3feb2a3ac419428d9a6ead9bb1c4d
Bryan Willett <btwillett2003@gmail.com>
2025-01-21 13:30:12 -0600

    added validations to models.

--- app/models/schedule_show.rb
+++ app/models/schedule_show.rb
@@ -1,4 +1,8 @@
 class ScheduleShow < ApplicationRecord
   belongs_to :schedule
   belongs_to :show
+
+  validates :schedule_id, presence: true
+  validates :show_id, presence: true
+  validates :show_id, uniqueness: { scope: :schedule_id, message: "is already added to this schedule" }
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,9 @@
 class Show < ApplicationRecord
   has_many :schedule_shows
   has_many :schedules, through: :schedule_shows
+
+  validates :artist, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
